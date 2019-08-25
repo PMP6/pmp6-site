ec2b8188beb4edec14f3f0de77b4eab268a2b319
Thibault Suzanne <thi.suzanne@gmail.com>
2019-08-25 16:15:59 +0200

    Remove photoswipe dep and configuration

--- assets/js/photoswipe.js
+++
@@ -1,5 +0,0 @@
-import PhotoSwipe from 'photoswipe';
-import PhotoSwipeUI_Default from 'photoswipe/dist/photoswipe-ui-default';
-
-window.PhotoSwipe = PhotoSwipe;
-window.PhotoSwipeUI_Default = PhotoSwipeUI_Default;
