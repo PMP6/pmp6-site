1a237b939c44bd0dbc6e782d219dadb8c46a8f3b
Thibault Suzanne <thi.suzanne@gmail.com>
2024-09-25 23:51:40 +0100

    Remove commented line

--- assets/js/app.js
+++ assets/js/app.js
@@ -14,5 +14,3 @@ window.moment = require('moment/moment');
 // If you want to pick and choose which modules to include, comment out the above and uncomment
 // the line below
 //import './lib/foundation-explicit-pieces';
-
-// $(document).foundation();
